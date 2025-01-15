33a98b7f4f7d6360b549d4510c01c0c23d5761f4
lokesh-yadav12 <lokesh916635@gmail.com>
2025-01-15 22:06:07 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import userRouter from './routes/user.route.js';
+
 const app = express();
 dotenv.config();
 
 app.use(cors());
+app.use(express.json()); // Middleware for parsing JSON
 
+// Database connection
 mongoose.connect(process.env.MONGO)
     .then(() => {
         console.log("DB Connected successfully!!");
@@ -15,7 +19,10 @@ mongoose.connect(process.env.MONGO)
         console.error("DB Connection failed:", err.message);
     });
 
-app.listen(3000,() =>{
-console.log('Server is running on port 3000!!!!'); 
-}
-)
+// Routes
+app.use('/api/user', userRouter);
+
+// Start server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!!!!');
+});
